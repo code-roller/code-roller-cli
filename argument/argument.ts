da1e902931a389ea042c5277112e0cb765ace1c1
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-03-29 21:46:03 +0530

    Added some commenst

--- argument/argument.ts
+++ argument/argument.ts
@@ -2,10 +2,32 @@ export class CodeRollerArguments {
     private readonly arguments:Array<string>
     private readonly length:number
 
+    /**
+     * @constructor
+     * 
+     * @param args The command line arguments passed in while
+     * running the cli which is passed in for parsing and executing'
+     * the commands accordingly
+     */
     constructor(args:Array<string>){
         this.arguments = args.slice(2, args.length)
         this.length = this.arguments.length
 
-        console.log(this.arguments)
+        this.beginArgumentParser(this.arguments, this.length)
+    }
+
+    /**
+     * @private
+     * 
+     * @param argument The arguments
+     * @param length The length of the argument
+     */
+    private beginArgumentParser = (argument:Array<string>, length:number):any => {
+        if(length == 0){
+            process.exit()
+        } else {
+            const command = argument[0]
+            console.log(argument)
+        }
     }
 }
